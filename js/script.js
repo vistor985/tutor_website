5278a61f03ae45fd00380774966575ab9609709a
LJM <1743390732@qq.com>
2025-04-27 16:54:52 +0800

    learn done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
   const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
@@ -5,4 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+});
+
+//Change navbar background on scroll
+window.addEventListener("scroll", function () {
+  const navbar = this.document.querySelector(".navbar");
+
+  if (this.window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
 });
